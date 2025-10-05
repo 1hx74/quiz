d30d6c2fc95c1d9cdf970490ad8fe36e3b403b7b
Deter35 <domozirovfedor@gmail.com>
2025-10-05 18:11:04 +0500

    add test on gerIdxAnswer

--- src/test/java/QuizTest.java
+++ src/test/java/QuizTest.java
@@ -1,10 +1,36 @@
+
+import org.example.Quiz;
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import java.io.InputStream;
+import java.io.ByteArrayInputStream;
 
 class QuizTest {
 
+public
     @Test
     void testAdd() {
         assertEquals(1, 1);
+    }
+
+    @Test
+    public void testPrivateMethodGetIdxAnswer() throws Exception {
+        String input = "A\n";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        Quiz quiz = new Quiz();
+        Method addMethod = Quiz.class.getDeclaredMethod("getIdxAnswer");
+        addMethod.setAccessible(true);
+
+        int index = (int) addMethod.invoke(quiz);
+        assertEquals(0, index);
+    }
+    @Test
+
+
+    public void testPrivateMethodQuizRun(){
+    //Method memory = Memory.class.getDeclaredMethod("Memory");
+
     }
 }
